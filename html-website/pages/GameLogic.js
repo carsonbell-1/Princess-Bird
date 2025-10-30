0592084cd50d85c43496885fd4efc5bc848a27ed
Annah Fenchak <arfenc0185@ung.edu>
2025-10-30 18:38:52 -0400

    Are we sure

---
+++ html-website/pages/GameLogic.js
@@ -0,0 +1,21 @@
+const backgrounds = [
+  'nature 1/orig.png',
+  'nature 2/orig.png',
+  'nature 3/orig.png',
+  'nature 4/orig.png'
+];
+
+let currentIndex = 0;
+
+function changeBackground() {
+  const gameArea = document.getElementById('game-area') || document.body;
+  gameArea.style.backgroundImage = `url(${backgrounds[currentIndex]})`;
+  gameArea.style.backgroundSize = 'cover';
+  gameArea.style.backgroundRepeat = 'no-repeat';
+  gameArea.style.backgroundPosition = 'center';
+  gameArea.style.transition = 'background 1s ease-in-out';
+  currentIndex = (currentIndex + 1) % backgrounds.length;
+}
+
+changeBackground();
+setInterval(changeBackground, 30000);
